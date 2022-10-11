9f9b54c4fcc442353c958a6d232606cd4e2e2930
Sundarrajan Raman <sundarrajan.raman@gmail.com>
2022-10-11 23:14:07 +0530

    Initial Commit

---
+++ mymaths-api/src/main/java/io/github/bahulprojects/SimpleMathsAPI.java
@@ -0,0 +1,19 @@
+package io.github.bahulprojects;
+
+public class SimpleMathsAPI {
+	public long add(int a, int b) {
+		return a + b;
+	}
+	
+	public long multiply(long a, long b) {
+		return a * b;
+	}
+	
+	public float divide (float a, float b) {
+		return a/b;
+	}
+	
+	public long subtract (long a, long b) {
+		return a - b;
+	}
+}
